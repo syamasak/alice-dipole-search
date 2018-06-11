378816cff1e531d772681e02ef58fa796aa135db
Shuto Yamasaki <shuto.yamasaki@cern.ch>
2018-06-11 22:47:14 +0900

    Improved build script

--- quick-segment-search/runme.sh
+++ quick-segment-search/runme.sh
@@ -1,14 +1,16 @@
+(cd `dirname $0`; time stack install)  # install genparam binary to ~/.local/bin
 {
   echo Started at `date`
-  time stack build
 
-  time stack exec genparam dip5k AliMagFastDip5k Sol30_Dip6_Hole.txt
+  echo genparam dip5k AliMagFastDip5k Sol30_Dip6_Hole.txt
+  time genparam dip5k AliMagFastDip5k Sol30_Dip6_Hole.txt
   mkdir -p AliMagFastDip5k
   mv AliMagFastDip5k.cxx AliMagFastDip5k
 
-  time stack exec genparam dip2k AliMagFastDip2k Sol12_Dip6_Hole.txt
+  echo genparam dip2k AliMagFastDip2k Sol12_Dip6_Hole.txt
+  time genparam dip2k AliMagFastDip2k Sol12_Dip6_Hole.txt
   mkdir -p AliMagFastDip2k
   mv AliMagFastDip2k.cxx AliMagFastDip2k
 
   echo Finished at `date`
-} | tee -a genparam.log
+} 2>&1 | tee -a genparam.log
